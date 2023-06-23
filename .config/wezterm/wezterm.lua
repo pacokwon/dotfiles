52f6f2e4fec463a3193ef118f5666b922232cff1
pacokwon <haechank@gmail.com>
2023-06-23 13:34:06 +0900

    fallback font for CJK input / nerd fonts

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,6 +2,15 @@ local wezterm = require 'wezterm'
 
 return {
     color_scheme = 'tokyonight_night',
-    colors = { cursor_bg = '#E6B450' },
-    font = wezterm.font('JetBrains Mono'),
+    colors = {
+        cursor_bg = '#E6B450',
+    },
+    -- color_scheme = 'foo',
+    font = wezterm.font_with_fallback({
+        'Victor Mono',
+        'Noto Sans Mono CJK KR',
+        'Symbols Nerd Font'
+    }),
+    font_size = 10,
+    use_ime = true,
 }
