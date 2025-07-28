e9c4739b507113b802429859835e176e32dd67a5
pacokwon <haechank@gmail.com>
2025-07-28 10:54:32 +0900

    nvim: add a newline after cursor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,6 +89,9 @@ vim.o.confirm = true
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- add a newline below the cursor
+vim.keymap.set('n', '<leader>o', 'o<ESC>k')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
