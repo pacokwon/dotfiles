3a8d3b0e8bef42ca86707971582cb4a01a89eb59
pacokwon <haechank@gmail.com>
2025-07-28 10:54:24 +0900

    nvim: tabstop 4

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -66,6 +66,7 @@ vim.o.splitbelow = true
 --   and `:help lua-options-guide`
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.tabstop = 4
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
