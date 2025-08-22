c1b9b452387064ddbcc101ae5a78cddf771bd8b7
pacokwon <haechank@gmail.com>
2025-08-22 23:36:35 +0900

    nvim: enable inlay hints by default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -525,6 +525,8 @@ require('lazy').setup({
           --
           -- This may be unwanted, since they displace some of your code
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            vim.lsp.inlay_hint.enable(true)
+
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
