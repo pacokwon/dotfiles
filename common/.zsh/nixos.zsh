752ff46223867c7b4056df4afc179cd75c4292df
pacokwon <haechank@gmail.com>
2026-02-14 01:22:22 +0900

    alacritty: use regular font and use fallback for icon

--- common/.zsh/nixos.zsh
+++ common/.zsh/nixos.zsh
@@ -41,8 +41,14 @@ export DELTA_FEATURES="+side-by-side line-numbers"
 
 alias vim="nvim"
 alias cat="bat"
-alias ls='eza --icons --color always'
-alias ll='eza --icons --color always -l'
+alias ls='eza --icons --color auto'
+alias ll='eza --icons --color auto -l'
 alias less='less -RFX' # display color on less
+
+# I'm using a symbols only nerd font as a fallback,
+# and an unpatched font as primary. the symbols look like
+# it's right next to the entries because it's bigger than other fonts
+# Adjust the spacing with this variable
+export EZA_ICON_SPACING=2
 
 eval "$(zoxide init zsh)"
