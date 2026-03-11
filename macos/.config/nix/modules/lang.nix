46f3b96827794c55c86ae5e3b6b3eea61fd34a80
pacokwon <haechank@gmail.com>
2026-03-11 14:29:45 +0900

    nix-darwin: latex packages

--- macos/.config/nix/modules/lang.nix
+++ macos/.config/nix/modules/lang.nix
@@ -4,10 +4,37 @@ let
     ps: with ps; [
       dvisvgm dvipng # for preview and export as html
       wrapfig amsmath ulem hyperref capt-of
+
+      # Add these for ACM and Korean support
+      acmart
+      hyperxmp
+      comment
+      inconsolata
+      environ     # Required by acmart
+      totpages    # Required by acmart
+      ifmtarg     # Required by acmart
+      ncctools    # Often required for many-column layouts in ACM
+      kotex-utf   # For \usepackage{kotex} support
+      xetex       # Recommended engine for kotex
+      collection-langkorean
+      lipsum
+
+      # Logic and String handling (Required by acmart)
+      xstring
+      etoolbox
+
+      # Fonts and Symbols
+      amsfonts
+      ncctools
+      cabin
+      libertine
+      sourcecodepro
+
+      latexmk
   ]));
 in
 {
-  environment.systemPackages = with pkgs; [ 
+  environment.systemPackages = with pkgs; [
     deno
     go
     rustc
