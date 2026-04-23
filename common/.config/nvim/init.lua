e09396a2dffdcc113c9c115f8a75c7831fc5570b
pacokwon <haechank@gmail.com>
2026-04-23 16:56:53 +0900

    nvim: trim trailing whitespace autocmd

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -280,6 +280,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    local view = vim.fn.winsaveview()
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.winrestview(view)
+  end,
+})
+
 vim.pack.add({
   'https://github.com/NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   'https://github.com/rightson/vim-p4-syntax',
@@ -341,140 +349,5 @@ require('which-key').setup {
   },
 }
 
--- -- INFO: plugins
--- -- we install plugins with neovim's builtin package manager: vim.pack
--- -- and then enable/configure them by calling their setup functions.
--- --
--- -- (see `:h vim.pack` for more details on how it works)
--- -- you can press `gx` on any of the plugin urls below to open them in your
--- -- browser and check out their documentation and functionality.
--- -- alternatively, you can run `:h {plugin-name}` to read their documentation.
--- --
--- -- plugins are then loaded and configured with a call to `setup` functions
--- -- provided by each plugin. this is not a rule of neovim but rather a convention
--- -- followed by the community.
--- -- these setup calls take a table as an agument and their expected contents can
--- -- vary wildly. refer to each plugin's documentation for details.
---
--- -- INFO: formatting and syntax highlighting
--- vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' }, { confirm = false })
---
--- -- equivalent to :TSUpdate
--- require('nvim-treesitter.install').update 'all'
---
--- require('nvim-treesitter.configs').setup {
---   auto_install = true, -- autoinstall languages that are not installed yet
--- }
---
--- -- INFO: completion engine
--- vim.pack.add({ 'https://github.com/saghen/blink.cmp' }, { confirm = false })
---
--- require('blink.cmp').setup {
---   completion = {
---     documentation = {
---       auto_show = true,
---     },
---   },
---
---   -- default blink keymaps
---   keymap = {
---     ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
---     ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
---
---     ['<C-y>'] = { 'select_and_accept', 'fallback' },
---     ['<C-e>'] = { 'cancel', 'fallback' },
---     ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
---
---     ['<Tab>'] = { 'snippet_forward', 'fallback' },
---     ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
---
---     ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
---     ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
---
---     ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
---   },
---
---   fuzzy = {
---     implementation = 'lua',
---   },
--- }
---
--- -- INFO: lsp server installation and configuration
---
--- -- lsp servers we want to use and their configuration
--- -- see `:h lspconfig-all` for available servers and their settings
--- local lsp_servers = {
---   lua_ls = {
---     -- https://luals.github.io/wiki/settings/ | `:h nvim_get_runtime_file`
---     Lua = { workspace = { library = vim.api.nvim_get_runtime_file('lua', true) } },
---   },
---   clangd = {},
---   rust_analyzer = {},
---   gopls = {},
--- }
---
--- vim.pack.add({
---   'https://github.com/neovim/nvim-lspconfig', -- default configs for lsps
---
---   -- NOTE: if you'd rather install the lsps through your OS package manager you
---   -- can delete the next three mason-related lines and their setup calls below.
---   -- see `:h lsp-quickstart` for more details.
---   'https://github.com/mason-org/mason.nvim', -- package manager
---   'https://github.com/mason-org/mason-lspconfig.nvim', -- lspconfig bridge
---   'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim', -- auto installer
--- }, { confirm = false })
---
--- require('mason').setup()
--- require('mason-lspconfig').setup()
--- require('mason-tool-installer').setup {
---   ensure_installed = vim.tbl_keys(lsp_servers),
--- }
---
--- -- configure each lsp server on the table
--- -- to check what clients are attached to the current buffer, use
--- -- `:checkhealth vim.lsp`. to view default lsp keybindings, use `:h lsp-defaults`.
--- for server, config in pairs(lsp_servers) do
---   vim.lsp.config(server, {
---     settings = config,
---
---     -- only create the keymaps if the server attaches successfully
---     on_attach = function(_, bufnr)
---       vim.keymap.set('n', 'grd', vim.lsp.buf.definition, { buffer = bufnr, desc = 'vim.lsp.buf.definition()' })
---
---       vim.keymap.set('n', 'grf', vim.lsp.buf.format, { buffer = bufnr, desc = 'vim.lsp.buf.format()' })
---     end,
---   })
--- end
---
--- -- INFO: fuzzy finder
--- vim.pack.add({
---   'https://github.com/nvim-lua/plenary.nvim', -- library dependency
---   'https://github.com/nvim-tree/nvim-web-devicons', -- icons (nerd font)
---   'https://github.com/nvim-telescope/telescope.nvim', -- the fuzzy finder
--- }, { confirm = false })
---
--- require('telescope').setup {}
---
--- local pickers = require 'telescope.builtin'
---
--- vim.keymap.set('n', '<leader>sp', pickers.builtin, { desc = '[S]earch Builtin [P]ickers' })
--- vim.keymap.set('n', '<leader>sb', pickers.buffers, { desc = '[S]earch [B]uffers' })
--- vim.keymap.set('n', '<leader>sf', pickers.find_files, { desc = '[S]earch [F]iles' })
--- vim.keymap.set('n', '<leader>sw', pickers.grep_string, { desc = '[S]earch Current [W]ord' })
--- vim.keymap.set('n', '<leader>sg', pickers.live_grep, { desc = '[S]earch by [G]rep' })
--- vim.keymap.set('n', '<leader>sr', pickers.resume, { desc = '[S]earch [R]esume' })
---
--- vim.keymap.set('n', '<leader>sh', pickers.help_tags, { desc = '[S]earch [H]elp' })
--- vim.keymap.set('n', '<leader>sm', pickers.man_pages, { desc = '[S]earch [M]anuals' })
---
--- -- INFO: keybinding helper
--- vim.pack.add({ 'https://github.com/folke/which-key.nvim' }, { confirm = false })
---
--- require('which-key').setup {
---   spec = {
---     { '<leader>s', group = '[S]earch', icon = { icon = '', color = 'green' } },
---   },
--- }
---
--- -- uncomment to enable automatic plugin updates
--- -- vim.pack.update()
+-- uncomment to enable automatic plugin updates
+-- vim.pack.update()
