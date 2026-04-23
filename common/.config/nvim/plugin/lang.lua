fc92daf12967248bac2ff2c59a7aa69b2de922c8
pacokwon <haechank@gmail.com>
2026-04-23 16:57:13 +0900

    nvim: render-markdown plugin

--- common/.config/nvim/plugin/lang.lua
+++ common/.config/nvim/plugin/lang.lua
@@ -1,3 +1,10 @@
 vim.pack.add { 'https://github.com/Julian/lean.nvim' }
 
 require('lean').setup { mappings = true }
+
+vim.pack.add({
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/nvim-tree/nvim-web-devicons',
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+})
+require('render-markdown').setup({})
