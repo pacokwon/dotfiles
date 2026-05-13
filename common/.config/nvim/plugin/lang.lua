66503ff5966bd66263b207f44bb0f04fd936bfa8
pacokwon <haechank@gmail.com>
2026-05-13 16:13:16 +0900

    nvim: coq

--- common/.config/nvim/plugin/lang.lua
+++ common/.config/nvim/plugin/lang.lua
@@ -1,10 +1,19 @@
-vim.pack.add { 'https://github.com/Julian/lean.nvim' }
+vim.pack.add {
+  'https://github.com/Julian/lean.nvim',
+  'https://github.com/whonore/coqtail',
+  'https://github.com/tomtomjhj/vsrocq.nvim',
+}
 
 require('lean').setup { mappings = true }
 
-vim.pack.add({
+vim.g.coqtail_treat_stderr_as_warning = 1
+vim.g.loaded_coqtail = 1
+vim.g['coqtail#supported'] = 0
+require('vsrocq').setup {}
+
+vim.pack.add {
   'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/nvim-tree/nvim-web-devicons',
   'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-})
-require('render-markdown').setup({})
+}
+require('render-markdown').setup {}
