a0037ad3f2b97b42e2f1cb94a4b1f9921da27257
pacokwon <haechank@gmail.com>
2026-08-05 11:29:54 +0900

    nvim: dprint for markdown

--- common/.config/nvim/plugin/format.lua
+++ common/.config/nvim/plugin/format.lua
@@ -24,7 +24,7 @@ require('conform').setup {
   end,
   formatters_by_ft = {
     lua = { 'stylua' },
-    -- markdown = { 'markdownlint-cli2' },
+    markdown = { 'dprint' },
     -- Conform can also run multiple formatters sequentially
     -- python = { "isort", "black" },
     --
